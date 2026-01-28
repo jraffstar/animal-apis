bc80c65e83f29685d9ce4f7ce79ccd7c02201a1b
Jenny <jraffstar@gmail.com>
2026-01-28 14:42:46 +0000

    Add functionality for cat images to be permenantly stored

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import requests
 import tkinter as tk
 from threading import *
 from PIL import Image, ImageTk
+import os
 
 f = open("API_KEY.txt", "r")
 API_KEY  = f.read()
+save_path = "images/cat.jpg"
+
+# Create directories used in program if they do not already exist
+newpath = r'images/'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+newpath = r'images/perm/'
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
 
 
 class tkinterApp(tk.Tk):
@@ -47,29 +58,46 @@ class home(tk.Frame):
 class catPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
-        # Fetch and download a random cat image
-        def one_random_image():
-            save_path = "images/cat.jpg"
-
-
+        def fetch_image():
             r = requests.get(f"https://api.thecatapi.com/v1/images/search?api_key={API_KEY}")
             image_json = r.json()
 
             image = requests.get(image_json[0]["url"])
 
             if image.status_code == 200:
-                with open(save_path ,"wb") as file:
+                with open(save_path, "wb") as file:
                     file.write(image.content)
                 print("File downloaded successfully")
-        
+
             else:
                 print("NO")
 
-        def show_image():
-            one_random_image()
+        # Check whether the user has enabled the permanent store option
+        def check_perm():
+            global save_path
+            print(bool_permanent_store.get())
+            if bool_permanent_store.get() == 1:
+                # Count how many images are in the directory currently
+                dir_path = "images/perm/"
+                self.count = 0
+                for path in os.listdir(dir_path):
+                    if os.path.isfile(os.path.join(dir_path, path)):
+                        self.count += 1
 
-            img = Image.open("images/cat.jpg")
+                save_path = f"images/perm/cat{self.count}.jpg"
+            else:
+                save_path = "images/cat.jpg"
+
+            fetch_image()
+
+        def show_image():
+            check_perm()
+
+            print(save_path)
+            if bool_permanent_store.get() == 1:
+                img = Image.open(f"images/perm/cat{self.count}.jpg")
+            else:
+                img = Image.open("images/cat.jpg")
 
             self.cat_image = ImageTk.PhotoImage(img)
 
@@ -86,6 +114,10 @@ class catPage(tk.Frame):
 
         display_image_button = tk.Button(self, text="Get random image", command=lambda:threading())
         display_image_button.pack()
+
+        bool_permanent_store = tk.IntVar()
+        permanent_store = tk.Checkbutton(self, text="Save images permanently?", variable=bool_permanent_store, onvalue=1)
+        permanent_store.pack()
 
         home_page_button = tk.Button(self, text="Home", command=lambda:controller.show_frame(home))
         home_page_button.pack()
@@ -128,7 +160,7 @@ class dogPage(tk.Frame):
             t1=Thread(target=show_dog_image)
             t1.start()
 
-            
+
         display_image_button = tk.Button(self, text="Get random image", command=lambda:threading())
         display_image_button.pack()
 
